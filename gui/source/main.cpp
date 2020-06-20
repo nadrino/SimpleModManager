bb2fade5e5a0b6e43ed2783ac15d38cbc5fb10a6
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-06-20 16:08:10 +0200

    - Now the GUI boots successfully. This commit mark the merge back to the master branch.  - Removing debug files/folders

--- gui/source/main.cpp
+++ gui/source/main.cpp
@@ -1,8 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-
-#include <toolbox.h> // keep it for debug
+#include <toolbox.h>
 
 #include <borealis.hpp>
+#include <string>
 #include <GlobalObjects.h>
 
 bool __is_new_version__;
@@ -10,8 +12,8 @@ void run_gui();
 void run_console();
 
 
-// MAIN
-int main(int argc, char **argv){
+int main(int argc, char* argv[])
+{
 
   toolbox::enableEmbeddedSwitchFS();
 
@@ -26,7 +28,7 @@ int main(int argc, char **argv){
       toolbox::split_string(
         GlobalObjects::get_mod_browser().get_parameters_handler().get_parameter("last-program-version"),
         "."
-        ),
+      ),
       ""
     )
   );
@@ -42,7 +44,6 @@ int main(int argc, char **argv){
     __is_new_version__ = true;
   }
 
-
   if(bool(std::stoi(GlobalObjects::get_mod_browser().get_parameters_handler().get_parameter("use-gui")))){
     run_gui();
   }
@@ -52,50 +53,120 @@ int main(int argc, char **argv){
 
   toolbox::disableEmbeddedSwitchFS();
 
+  // Exit
   return EXIT_SUCCESS;
-
 }
 
-static void errorCallback(int errorCode, const char* description)
-{
-  brls::Logger::error("[GLFW:%d] %s", errorCode, description);
-}
 
 void run_gui(){
 
   // Init the app
   brls::Logger::setLogLevel(brls::LogLevel::DEBUG);
 
-  glfwSetErrorCallback(errorCallback);
-  glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE);
-  if (!glfwInit())
+  if (!brls::Application::init("SimpleModManager"))
   {
-//    Logger::error("Failed to initialize glfw");
-//    return false;
+    brls::Logger::error("Unable to init Borealis application");
+    exit(EXIT_FAILURE);
   }
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE);
-  glfwCreateWindow(1280, 720, "SimpleModManager", nullptr, nullptr);
-  exit(EXIT_SUCCESS);
-//
-//  if (!brls::Application::init("SimpleModManager")){
-//    brls::Logger::error("Unable to init Borealis application");
-//    exit(EXIT_FAILURE);
-//  }
 
   // Create a sample view
-//  auto* rootFrame = new brls::TabFrame();
-//  rootFrame->setTitle("SimpleModManager");
-//  rootFrame->setIcon(BOREALIS_ASSET("icon/borealis.jpg"));
+  brls::TabFrame* rootFrame = new brls::TabFrame();
+  rootFrame->setTitle("SimpleModManager");
+  rootFrame->setIcon("romfs:/icon/icon.png");
+
+  brls::List* testList = new brls::List();
+
+  brls::ListItem* dialogItem = new brls::ListItem("Open a dialog");
+  dialogItem->getClickEvent()->subscribe([](brls::View* view) {
+    brls::Dialog* dialog = new brls::Dialog("Warning: PozzNX will wipe all data on your Switch and render it inoperable, do you want to proceed?");
+
+    brls::GenericEvent::Callback closeCallback = [dialog](brls::View* view) {
+      dialog->close();
+      brls::Application::notify("Running PozzNX...");
+    };
+
+    dialog->addButton("Continue", closeCallback);
+    dialog->addButton("Continue", closeCallback);
+    dialog->addButton("Continue", closeCallback);
+
+    dialog->setCancelable(false);
+
+    dialog->open();
+  });
+
+  brls::ListItem* themeItem = new brls::ListItem("TV Resolution");
+  themeItem->setValue("Automatic");
+
+  brls::SelectListItem* jankItem = new brls::SelectListItem(
+    "User Interface Jank",
+    { "Native", "Minimal", "Regular", "Maximum", "SX OS", "Windows Vista", "iOS 14" });
+
+  brls::ListItem* crashItem = new brls::ListItem("Divide by 0", "Can the Switch do it?");
+  crashItem->getClickEvent()->subscribe([](brls::View* view) { brls::Application::crash("The software was closed because an error occured:\nSIGABRT (signal 6)"); });
+
+  brls::ListItem* popupItem = new brls::ListItem("Open popup");
+  popupItem->getClickEvent()->subscribe([](brls::View* view) {
+    brls::TabFrame* popupTabFrame = new brls::TabFrame();
+    popupTabFrame->addTab("Red", new brls::Rectangle(nvgRGB(255, 0, 0)));
+    popupTabFrame->addTab("Green", new brls::Rectangle(nvgRGB(0, 255, 0)));
+    popupTabFrame->addTab("Blue", new brls::Rectangle(nvgRGB(0, 0, 255)));
+    brls::PopupFrame::open("Popup title", BOREALIS_ASSET("icon/borealis.jpg"), popupTabFrame, "Subtitle left", "Subtitle right");
+  });
+
+  brls::SelectListItem* layerSelectItem = new brls::SelectListItem("Select Layer", { "Layer 1", "Layer 2" });
+
+  testList->addView(dialogItem);
+  testList->addView(themeItem);
+  testList->addView(jankItem);
+  testList->addView(crashItem);
+  testList->addView(popupItem);
+
+  brls::Label* testLabel = new brls::Label(brls::LabelStyle::REGULAR, "For more information about how to use Nintendo Switch and its features, please refer to the Nintendo Support Website on your smart device or PC.", true);
+  testList->addView(testLabel);
+
+  brls::ListItem* actionTestItem = new brls::ListItem("Custom Actions");
+  actionTestItem->registerAction("Show notification", brls::Key::L, [] {
+    brls::Application::notify("Custom Action triggered");
+    return true;
+  });
+  testList->addView(actionTestItem);
+
+  brls::LayerView* testLayers = new brls::LayerView();
+  brls::List* layerList1      = new brls::List();
+  brls::List* layerList2      = new brls::List();
+
+  layerList1->addView(new brls::Header("Layer 1", false));
+  layerList1->addView(new brls::ListItem("Item 1"));
+  layerList1->addView(new brls::ListItem("Item 2"));
+  layerList1->addView(new brls::ListItem("Item 3"));
+
+  layerList2->addView(new brls::Header("Layer 2", false));
+  layerList2->addView(new brls::ListItem("Item 1"));
+  layerList2->addView(new brls::ListItem("Item 2"));
+  layerList2->addView(new brls::ListItem("Item 3"));
+
+  testLayers->addLayer(layerList1);
+  testLayers->addLayer(layerList2);
+
+  layerSelectItem->getValueSelectedEvent()->subscribe([=](size_t selection) {
+    testLayers->changeLayer(selection);
+  });
+
+  testList->addView(layerSelectItem);
+
+  rootFrame->addTab("First tab", testList);
+  rootFrame->addTab("Second tab", testLayers);
+  rootFrame->addSeparator();
+  rootFrame->addTab("Third tab", new brls::Rectangle(nvgRGB(255, 0, 0)));
+  rootFrame->addTab("Fourth tab", new brls::Rectangle(nvgRGB(0, 255, 0)));
 
   // Add the root view to the stack
-//  brls::Application::pushView(rootFrame);
+  brls::Application::pushView(rootFrame);
 
-//  while (brls::Application::mainLoop()){
-    // do something ?
-//  }
+  // Run the app
+  while (brls::Application::mainLoop())
+    ;
+
 
 }
 
