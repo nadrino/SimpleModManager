73a6639462d4f6a0d4e8337a3b208ac210a21378
Witherking25 <aaronkoonce25@gmail.com>
2021-02-05 12:23:28 -0600

    gave plugins their own file extension and actually filter the plugins menu

--- gui/include/mod_browser/tab_mod_plugins.h
+++ gui/include/mod_browser/tab_mod_plugins.h
@@ -3,29 +3,28 @@
 
 #include <borealis.hpp>
 
-class tab_mod_plugins : public brls::List {
+class tab_mod_plugins : public brls::List
+{
 
 public:
-  tab_mod_plugins();
+	tab_mod_plugins();
 
-  std::map<std::string, brls::ListItem *> &getModsListItems();
+	std::map<std::string, brls::ListItem *> &getModsListItems();
 
-  void draw(NVGcontext* vg, int x, int y, unsigned width, unsigned height, brls::Style* style, brls::FrameContext* ctx) override;
+	void draw(NVGcontext *vg, int x, int y, unsigned width, unsigned height, brls::Style *style, brls::FrameContext *ctx) override;
 
-  std::string remove_extension(const std::string& filename);
-  brls::Image* load_image_cache(std::string filename);
+	std::string remove_extension(const std::string &filename);
+	std::string get_extension(const std::string &filename);
+	brls::Image *load_image_cache(std::string filename);
 
-  std::string base64_encode(const std::string& in);
-  std::string base64_decode(const std::string& in);
+	std::string base64_encode(const std::string &in);
+	std::string base64_decode(const std::string &in);
 
 private:
-  brls::Dialog* dialog;
-  std::map<std::string, brls::ListItem*> _modsListItems_;
-  int frameCounter;
-
-
-
+	brls::Dialog *dialog;
+	std::map<std::string, brls::ListItem *> _modsListItems_;
+	int frameCounter;
 };
-extern std::map<std::string, brls::Image*> cached_thumbs;
+extern std::map<std::string, brls::Image *> cached_thumbs;
 
 #endif //SIMPLEMODMANAGER_TAB_MOD_PLUGINS_H
